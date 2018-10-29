bfb33bba23800f884003a251d78ad941ecb2e653
Nick Santoro <nicholassantoro@Nicholass-MacBook-Pro.local>
2018-10-29 16:47:03 -0700

    connecting heroku

--- knexfile.js
+++ knexfile.js
@@ -18,7 +18,7 @@ module.exports = {
     production: {
       client: 'pg',
       connection:
-        'na',
+        process.env.DATABASE_URL,
       migrations: {
         directory: './db/migrations'
       },
